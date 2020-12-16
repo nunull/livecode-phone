5e1ebf253d9421406e0021b5e0d5614c38176958
Timm Albers <timmm.albers@gmail.com>
2020-12-16 10:51:37 +0100

    Add basic dialog for adding transformations

--- public/index.js
+++ public/index.js
@@ -1,10 +1,20 @@
 const socket = io();
 
+Vue.component('add-transformation-dialog', {
+  template: `
+    <div class="add-transformation-dialog">
+      <transformation-view
+        v-for="transformation in $root.availableTransformations"
+        v-bind:transformation="transformation"
+      ></transformation-view>
+    </div>
+  `
+});
+
 Vue.component('pattern-view', {
   props: ['pattern'],
   data: function () {
     const height = 120;
-    console.log(this)
     return {
       stepHeight: 120,
       valueHeight: 120/this._props.pattern.scaleSteps
@@ -12,7 +22,7 @@ Vue.component('pattern-view', {
   },
   methods: {
     stepTopMargin: function (step) {
-      return (this._props.pattern.scaleSteps - step - 1 ) * this._data.valueHeight;
+      return (this._props.pattern.scaleSteps - step - 1) * this._data.valueHeight;
     },
   },
   template: `
@@ -22,34 +32,62 @@ Vue.component('pattern-view', {
       </div>
     </div>
   `
-})
+});
 
 Vue.component('transformation-view', {
-  props: ['transformation', 'isSubTransformation'],
+  props: ['transformation', 'isInstance', 'isSubTransformation'],
   template: `
     <div class="transformation">
-      <img src="/assets/arrow-down.png" class="arrow" v-if="!isSubTransformation">
+      <img src="/assets/arrow-down.png" class="arrow" v-if="isInstance && !isSubTransformation">
       <div class="value" v-bind:class="{ 'has-sub-transformation': !!transformation.transformation }">
         {{ transformation.name }}
         {{ transformation.value }}
-        <div class="sub-transformation" v-if="transformation.transformation">
+        <div class="sub-transformation" v-if="transformation.needsSubTransformation">
           <transformation-view
+            v-if="transformation.transformation"
             v-bind:transformation="transformation.transformation"
             v-bind:isSubTransformation="true"
           ></transformation-view>
+          <transformation-placeholder-view v-else></transformation-placeholder-view>
         </div>
       </div>
     </div>
   `
+});
+
+Vue.component('transformation-placeholder-view', {
+  template: `
+    <div class="transformation-placeholder">transformation</div>
+  `
 })
+
+Vue.component('round-button', {
+  props: ['text'],
+  template: `
+    <div class="round-button" v-on:click="$emit('click')">
+      {{ text }}
+    </div>
+  `
+});
 
 const app = new Vue({
   el: '#app',
   data: {
+    currentModalDialogComponent: null,
     pattern: { steps: [5, 0, 3, 2], scaleSteps: 6 },
+    availableTransformations: [
+      { name: 'pitch' },
+      { name: 'every', needsSubTransformation: true },
+      { name: 'chance' }
+    ],
     transformations: [
       { name: 'pitch', value: 2 },
-      { name: 'every', value: 2, transformation: { name: 'chance', value: 0.5 } }
+      { name: 'every', value: 2, needsSubTransformation: true, transformation: { name: 'chance', value: 0.5 } }
     ]
+  },
+  methods: {
+    modalDialog: function (componentName) {
+      this.currentModalDialogComponent = componentName
+    }
   }
-})
+});
